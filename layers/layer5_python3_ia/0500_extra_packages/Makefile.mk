605dd6089b60214469ae384bf21d19d5b94cd0e9
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-10-15 16:03:22 +0200

    feat: compatibility with layer python3_scientific@mfext (#72)
    Co-authored-by: Fabien MARTY <fabien.marty@gmail.com>

--- layers/layer5_python3_ia/0500_extra_packages/Makefile.mk
+++ layers/layer5_python3_ia/0500_extra_packages/Makefile.mk
@@ -1,6 +1,6 @@
 include ../../../adm/root.mk
 
-export HDF5_DIR=$(MFEXT_HOME)/opt/scientific
-export GEOS_DIR=$(MFEXT_HOME)/opt/scientific
+export HDF5_DIR=$(MFEXT_HOME)/opt/scientific_core
+export GEOS_DIR=$(MFEXT_HOME)/opt/scientific_core
 
 include $(MFEXT_HOME)/share/python3_layer.mk
