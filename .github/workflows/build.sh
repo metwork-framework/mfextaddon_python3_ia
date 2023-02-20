655ae4a3316d97c7a53b4b5aeed91aeda01707f8
metworkbot <metworkbot@metwork-framework.org>
2023-02-20 15:08:01 +0000

    build: sync common files from github_organization_management repository

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -11,6 +11,9 @@ export DRONE_TAG=${TAG}
 export DRONE=true
 
 
+#Security in case where previous build has been canceled (unwanted outputs may have remained)
+rm -rf html_doc rpms .build_hash
+
 
     if test "${OS_VERSION}" = "centos8"; then export METWORK_BUILD_OS=generic; else export METWORK_BUILD_OS=${OS_VERSION}; fi
 
@@ -41,6 +44,16 @@ if test "${OUTPUT}" != ""; then
     exit 1
 fi
 
+
+MODULEHASH=`/opt/metwork-mfext-${TARGET_DIR}/bin/mfext_wrapper module_hash 2>module_hash.debug`
+if test -f /opt/metwork-mfext-${TARGET_DIR}/.dhash; then cat /opt/metwork-mfext-${TARGET_DIR}/.dhash; fi
+cat module_hash.debug |sort |uniq ; rm -f module_hash.debug
+echo "${MODULEHASH}${DRONE_TAG}${DRONE_BRANCH}" |md5sum |cut -d ' ' -f1 >.build_hash
+if test -f "${BUILDCACHE}/build_hash_mfextaddon_python3_ia_${BRANCH}_`cat .build_hash`"; then
+    echo "bypass=true" >> github_output
+    echo "buildcache=null" >> github_output
+    exit 0
+fi
  
 
 if test -d docs; then make docs >${BUILDLOGS}/make_doc.log 2>&1 || ( tail -200 ${BUILDLOGS}/make_doc.log ; exit 1 ); fi
@@ -53,6 +66,14 @@ make RELEASE_BUILD=${GITHUB_RUN_NUMBER} rpm >${BUILDLOGS}/make_rpm.log 2>&1 || (
 mkdir rpms
 mv /opt/metwork-mfext-${TARGET_DIR}/*.rpm rpms
 
+
+rm -f ${BUILDCACHE}/build_hash_mfextaddon_python3_ia_${BRANCH}_*
+hash_file=${BUILDCACHE}/build_hash_mfextaddon_python3_ia_${BRANCH}_`cat .build_hash`
+touch ${hash_file}
+rm -f ./build_hash
+chown 1018:1018 ${hash_file}
+chmod 664 ${hash_file}
+echo "buildcache=${hash_file}" >> github_output
  
 
 echo "bypass=false" >> github_output
