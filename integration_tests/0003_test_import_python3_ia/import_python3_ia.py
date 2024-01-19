8026d674bbb871c978a80340cb2bd457237c3da0
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-01-19 16:45:35 +0100

    feat: add lightning, torchmetrics, torchvision, transformers, diffusers (#130)
    einops, onnx... and dependencies

--- integration_tests/0003_test_import_python3_ia/import_python3_ia.py
+++ integration_tests/0003_test_import_python3_ia/import_python3_ia.py
@@ -1,9 +1,15 @@
 # import tensorflow_io_gcs_filesystem
 import absl #absl_py
 import astunparse
+import diffusers
+import efficientnet_pytorch
+import einops
 import flatbuffers
 import google_auth_oauthlib
 import google #google_auth
+import huggingface_hub
+import lightning_utilities
+import lightning
 import pasta #google_pasta
 import grpc #grpcio
 import clang #libclang
@@ -20,15 +26,28 @@ from nvidia import curand
 from nvidia import cusolver
 from nvidia import cusparse
 from nvidia import nccl
+from nvidia import nvjitlink
 from nvidia import nvtx
+import onnx
 import oauthlib
+import onnx
+import onnxscript
 import opt_einsum
+import pretrainedmodels
+import pytorch_lightning
 import requests_oauthlib
 import rsa
+import safetensors
+import segmentation_models_pytorch
 import sympy
 import tensorboard_data_server
 import tensorboard
-import tensorflow_estimator
-import triton
-import torch
 import tensorflow
+import tensorflow_estimator
+import timm
+import tokenizers
+import torch
+import torchmetrics
+import torchvision
+import transformers
+import triton
