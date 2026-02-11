8a39de174e9b5540479fabe8e7d130e56695112f
thebaptiste <jean-baptiste.veslin@meteo.fr>
2026-02-11 17:28:42 +0000

    build: add missing imports for test

--- integration_tests/0003_test_import_python3_ia/import_python3_ia.py
+++ integration_tests/0003_test_import_python3_ia/import_python3_ia.py
@@ -1,19 +1,29 @@
 import absl #absl_py
+import antlr4 #antlr4-python3-runtime
 import astunparse
+import axial_attention
+import dataclasses_json
 import diffusers
 import tree #dm_tree
 import einops
 import flatbuffers
 import google_auth_oauthlib
 import google #google_auth
-import huggingface_hub
-import lightning_utilities
-import lightning
 import pasta #google_pasta
 import grpc #grpcio
-import clang #libclang
+import huggingface_hub
+import hydra #hydra-core
+import importlib_resources
+import jsonargparse
+import _jsonnet #jsonnet
 import keras
+import clang #libclang
+import lightning_utilities
+import lightning
+import marshmallow
+import mfai
 import ml_dtypes
+import monai
 import mpmath
 import namex
 from nvidia import cublas
@@ -31,6 +41,7 @@ from nvidia import nvjitlink
 from nvidia import nvshmem
 from nvidia import nvtx
 import oauthlib
+import omegaconf
 import onnx
 import onnxruntime
 import onnxscript
@@ -41,16 +52,21 @@ import requests_oauthlib
 import rsa
 import safetensors
 import segmentation_models_pytorch
+import sentencepiece
 import sympy
+import tabulate
 import tensorboard_data_server
 import tensorboard
 import tensorflow
 import tensorflow_hub
 import tf_keras
+import tiktoken
 import timm
 import tokenizers
 import torch
+import torch_geometric
 import torchmetrics
 import torchvision
 import transformers
 import triton
+import typeshed_client
