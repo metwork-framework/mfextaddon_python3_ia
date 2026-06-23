5f9cbf57e4c3447a4eca98a8170089204875542e
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2026-06-23 15:16:05 +0200

    feat: add mfai 7.3.0 (#279)

--- integration_tests/0003_test_import_python3_ia/import_python3_ia.py
+++ integration_tests/0003_test_import_python3_ia/import_python3_ia.py
@@ -1,20 +1,30 @@
 import absl #absl_py
+import alembic
 import antlr4 #antlr4-python3-runtime
 import astunparse
 import axial_attention
 import bitsandbytes
 from cuda import bindings
 from cuda import pathfinder
+import databricks.sdk
 import dataclasses_json
 import diffusers
+import docker
 import tree #dm_tree
 import einops
+import fastapi
+import flask_cors
 import flatbuffers
 import google_auth_oauthlib
 import google #google_auth
 import pasta #google_pasta
+import graphene
+import graphql #graphql_core
+import graphql_relay
 import grpc #grpcio
+import gunicorn
 import hf_xet
+import huey
 import huggingface_hub
 import hydra #hydra-core
 import jsonargparse
@@ -25,8 +35,9 @@ import lightning_fabric
 import lightning_utilities
 import lightning
 import marshmallow
-#import mfai
+import mfai
 import ml_dtypes
+import mlflow
 import monai
 import mpmath
 import namex
@@ -40,14 +51,20 @@ import omegaconf
 import onnx
 import onnxruntime
 import onnxscript
+import opentelemetry #opentelemetry_api
 import opt_einsum
 import optree
+import prettytable
+import dotenv #python_dotenv
 import pytorch_lightning
 import requests_oauthlib
 import rsa
 import safetensors
 import segmentation_models_pytorch
 import sentencepiece
+import skops
+import sqlparse
+import starlette
 import sympy
 import tabulate
 import tensorboard_data_server
@@ -65,3 +82,4 @@ import torchvision
 import transformers
 import triton
 import typeshed_client
+import uvicorn
