13a4f1ebd957013326d2e0998f14f2e416bcf592
thebaptiste <jean-baptiste.veslin@meteo.fr>
2026-08-19 04:41:38 +0000

    build: remove mlflow dependencies (not required anymore by mfai)

--- integration_tests/0003_test_import_python3_ia/import_python3_ia.py
+++ integration_tests/0003_test_import_python3_ia/import_python3_ia.py
@@ -1,30 +1,20 @@
 import absl #absl_py
-import alembic
 import antlr4 #antlr4-python3-runtime
 import astunparse
 import axial_attention
 import bitsandbytes
 from cuda import bindings
 from cuda import pathfinder
-import databricks.sdk
 import dataclasses_json
 import diffusers
-import docker
 import tree #dm_tree
 import einops
-import fastapi
-import flask_cors
 import flatbuffers
 import google_auth_oauthlib
 import google #google_auth
 import pasta #google_pasta
-import graphene
-import graphql #graphql_core
-import graphql_relay
 import grpc #grpcio
-import gunicorn
 import hf_xet
-import huey
 import huggingface_hub
 import hydra #hydra-core
 import jsonargparse
@@ -51,20 +41,14 @@ import omegaconf
 import onnx
 import onnxruntime
 import onnxscript
-import opentelemetry #opentelemetry_api
 import opt_einsum
 import optree
-import prettytable
-import dotenv #python_dotenv
 import pytorch_lightning
 import requests_oauthlib
 import rsa
 import safetensors
 import segmentation_models_pytorch
 import sentencepiece
-import skops
-import sqlparse
-import starlette
 import sympy
 import tabulate
 import tensorboard_data_server
@@ -82,4 +66,3 @@ import torchvision
 import transformers
 import triton
 import typeshed_client
-import uvicorn
