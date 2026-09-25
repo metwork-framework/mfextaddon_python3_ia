e741b84e06ed22daef13211fdf919739fc90f1a1
thebaptiste <jean-baptiste.veslin@meteo.fr>
2026-09-25 14:36:22 +0000

    build: remove packages not Python 3.15 compliant

--- integration_tests/0003_test_import_python3_ia/import_python3_ia.py
+++ integration_tests/0003_test_import_python3_ia/import_python3_ia.py
@@ -1,9 +1,9 @@
 import absl #absl_py
 import antlr4 #antlr4-python3-runtime
 import astunparse
-import axial_attention
-import bitsandbytes
-from cuda import bindings
+#import axial_attention (because of torch)
+#import bitsandbytes (because of torch)
+#from cuda import bindings (not Python 3.15 compliant)
 from cuda import pathfinder
 import dataclasses_json
 import diffusers
@@ -19,15 +19,15 @@ import huggingface_hub
 import hydra #hydra-core
 import jsonargparse
 import _jsonnet #jsonnet
-import keras
+#import keras (because of tensorflow)
 import clang #libclang
-import lightning_fabric
+#import lightning_fabric (because of torch)
 import lightning_utilities
-import lightning
+#import lightning (because of torch)
 import marshmallow
-import mfai
+#import mfai (because of onnxruntime)
 import ml_dtypes
-import monai
+#import monai (not Python 3.15 compliant)
 import mpmath
 import namex
 from nvidia import cudnn
@@ -38,30 +38,30 @@ import nvidia
 import oauthlib
 import omegaconf
 import onnx
-import onnxruntime
+#import onnxruntime (not Python 3.15 compliant)
 import onnxscript
 import opt_einsum
 import optree
-import pytorch_lightning
+#import pytorch_lightning (because of torch)
 import requests_oauthlib
 import rsa
 import safetensors
-import segmentation_models_pytorch
+#import segmentation_models_pytorch (because of torch)
 import sentencepiece
 import sympy
 import tabulate
 import tensorboard_data_server
 import tensorboard
 import tensorboardX
-import tensorflow
-import tf_keras
+#import tensorflow (not Python 3.15 compliant)
+#import tf_keras (because of tensorflow)
 import tiktoken
-import timm
+#import timm (because of torch)
 import tokenizers
-import torch
-import torch_geometric
-import torchmetrics
-import torchvision
+#import torch (not Python 3.15 compliant)
+#import torch_geometric (because of torch)
+#import torchmetrics (because of torch)
+#import torchvision (not Python 3.15 compliant)
 import transformers
-import triton
+#import triton (not Python 3.15 compliant)
 import typeshed_client
